dc7967f765e1809cce2377112c21f1b5088389f5
AreebKhanAnwar <areebkhanp1@gmail.com>
2024-09-24 22:44:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,357 @@
+// chapter # 03
+//  Question No.1
+
+// var age = 17;
+// alert("I am " + age + " year old");
+
+
+
+
+
+
+//  Question No.2
+
+// var visit = 12;
+// alert("you have visited this site " + visit + " times");
+
+
+
+
+
+
+//  Question No.3
+
+// var DOY = 2007;
+// document.write("My birth year is " + DOY);
+
+
+
+
+
+
+//  Question No.4
+
+// var visitors_name = "John Doe";
+// var product = "T-Shirt";
+// var Quantity = 5;
+// document.write(visitors_name + " Ordered " + Quantity +  product + " on XYZ Clothing store");
+
+
+
+
+
+
+
+// chapter # 04
+//  Question No.1
+
+// var a;
+// var b;
+// var c;
+
+
+
+
+
+
+
+//  Question No.2
+
+// Legal Name
+
+// var areeb;
+// var areeb01;
+// var _areeb;
+// var $areeb;
+// var a;
+
+
+// Illegal Name
+
+// var for;
+// var areeb khan;
+// var 01areeb;
+// var +areeb;
+// var @areeb;
+
+
+
+
+
+
+
+//  Question No.3
+// document.write("<h1>Rules for naming JS variables</h1>   <br/>");
+// document.write("Variable names can only contain number, $ , alphabets and underscore(_).  <br/> ");
+// document.write("Variables must begin with a letter , _ or $.  <br/> ");
+// document.write("Variable names are case Sensitive  <br/> ");
+// document.write("Variable names should not be JS Keywords");
+
+
+
+
+
+
+// chapter # 05
+//  Question No.1
+
+// var no1 = 3;
+// var no2 = 5;
+// var sum = no1 + no2;
+// document.write("The sum of "+  no1 + " and " + no2 +  " is " + sum);
+
+
+
+
+
+
+//  Question No.2
+
+// var no1 = 3;
+// var no2 = 5;
+// var diff = no1 - no2;
+// document.write("The difference of "+  no1 + " and " + no2 +  " is " + diff);
+
+
+// var no1 = 3;
+// var no2 = 5;
+// var division = no1 / no2;
+// document.write("The division of " +  no1 + " and " + no2 +  " is " + division);
+
+
+// var no1 = 3;
+// var no2 = 5;
+// var product = no1 * no2;
+// document.write("The product of "+  no1 + " and " + no2 +  " is " + product);
+
+
+
+
+
+
+//  Question No.3
+
+// var num;
+// console.log(num);
+
+// num = 20;
+// console.log("Initail value is" + num);
+
+// num++;
+// console.log("After increment " + num);
+
+// num += 7;
+// console.log("After adding some value " + num);
+
+// num--;
+// console.log("After decrement " + num);
+
+// num /= 3;
+// console.log("After divided by 3 is " + num);
+
+
+
+
+
+
+//  Question No.4
+
+// var TP = 600;
+// var NOT = 5;
+// document.write("Total cost of buy " + NOT + " tickets to a movie is " + TP*NOT + "PKR")
+
+
+
+
+
+
+//  Question No.5
+
+// document.write("<h2>Table of 2</h2>");
+// document.write("2 X 1 = " + 2*1 + "<br/>");
+// document.write("2 X 2 = " + 2*2 + "<br/>");
+// document.write("2 X 3 = " + 2*3 + "<br/>");
+// document.write("2 X 4 = " + 2*4 + "<br/>");
+// document.write("2 X 5 = " + 2*5 + "<br/>");
+// document.write("2 X 6 = " + 2*6 + "<br/>");
+// document.write("2 X 7 = " + 2*7 + "<br/>");
+// document.write("2 X 8 = " + 2*8 + "<br/>");
+// document.write("2 X 9 = " + 2*9 + "<br/>");
+// document.write("2 X 10 = " + 2*10 + "<br/>");
+
+
+
+
+
+
+//  Question No.6
+
+
+// var degF = 77;
+
+// var degC =  (degF - 32)*5/9;
+
+// document.write(degC + "&degF is " + degF + "&degC");
+
+
+// var degC = 40;
+
+// var degF =  (degC * 5/9) + 32;
+// document.write(degF + "&degC is " + degC + "&degF");
+
+
+
+
+
+
+//  Question No.7
+
+// var item1 = 650;
+// var QOI1 = 3;
+// var item2 = 100;
+// var QOI2 = 7;
+// var shiphingCharges = 100;
+
+// document.write("Price of Item 1 is " + item1 + "<br/>");
+// document.write("Quntity of Item 1 is " + QOI1 + "<br/>");
+// document.write("Price of Item 2 is " + item2 + "<br/>");
+// document.write("Quntity of Item 2 is " + QOI2 + "<br/>");
+// document.write("Shiping charges" + shiphingCharges + "<br/>");
+
+// document.write("Total amount of your order is " + (item1*QOI1+item2*QOI2+shiphingCharges));
+
+
+
+
+
+
+//  Question No.8
+
+// var TotalMarks = 980;
+// var obtainedmarks = 804;
+
+// document.write("Total Marks = " + TotalMarks + "<br/>");
+// document.write("Obtaied Marks = " + obtainedmarks + "<br/>");
+// document.write("Parcentage = " + obtainedmarks / TotalMarks * 100 )
+
+
+
+
+
+
+//  Question No.8
+
+// var USD = 10;
+// var SAR = 25;
+// var USDtoPKR = 104.80;
+// var SARtoPKR = 28;
+
+// var total = USD*USDtoPKR + SAR*SARtoPKR;
+// document.write("Total Currency in PKR " + total);
+
+
+
+
+
+
+// chapter # 06
+
+// Question No.1
+
+//  var a = 10;
+//  document.write("The value of a is: " + a + "<br/>" + "............................................" + "<br/>" + "<br/>");
+// ++a;
+// document.write("The value of ++a is: " + a + "<br/>");
+// document.write("Now the value of a is: " + a + "<br/>" + "<br/>");
+// document.write("The value of a++ is: " + a + "<br/>");
+// a++;
+// document.write("Now the value of a is: " + a + "<br/>" + "<br/>");
+// --a;
+// document.write("The value of --a is: " + a + "<br/>");
+// document.write("Now the value of a is: " + a + "<br/>" + "<br/>");
+// document.write("The value of a-- is: " + a + "<br/>");
+// a--;
+// document.write("Now the value of a is: " + a + "<br/>");
+
+
+
+
+
+// Question No.2
+
+// var a = 2;
+// var b = 1;
+
+// var result = --a - --b + ++b + b--;
+
+// document.write("a is " + a + "<br/>");
+// document.write("b is " + b + "<br/>");
+// document.write("Result is " + result);
+
+
+
+
+
+// Question No.3
+
+// var hi =prompt("Your Name");
+// document.write("Hello " + hi )
+
+
+
+
+
+
+// Question No.4
+
+// var input = prompt("Write a any Number");
+
+// if (input !== "" ) {
+
+//     document.write(input + " X 1 = " + input * 1 + "<br/>");
+//     document.write(input + " X 2 = " + input * 2 + "<br/>");
+//     document.write(input + " X 3 = " + input * 3 + "<br/>");
+//     document.write(input + " X 4 = " + input * 4 + "<br/>");
+//     document.write(input + " X 5 = " + input * 5 + "<br/>");
+//     document.write(input + " X 6 = " + input * 6 + "<br/>");
+//     document.write(input + " X 7 = " + input * 7 + "<br/>");
+//     document.write(input + " X 8 = " + input * 8 + "<br/>");
+//     document.write(input + " X 9 = " + input * 9 + "<br/>");
+//     document.write(input + " X 10 = " + input * 10 + "<br/>");
+
+// } else {
+
+//     document.write(5 + " X 1 = " + 5 * 1 + "<br/>");
+//     document.write(5 + " X 2 = " + 5 * 2 + "<br/>");
+//     document.write(5 + " X 3 = " + 5 * 3 + "<br/>");
+//     document.write(5 + " X 4 = " + 5 * 4 + "<br/>");
+//     document.write(5 + " X 5 = " + 5 * 5 + "<br/>");
+//     document.write(5 + " X 6 = " + 5 * 6 + "<br/>");
+//     document.write(5 + " X 7 = " + 5 * 7 + "<br/>");
+//     document.write(5 + " X 8 = " + 5 * 8 + "<br/>");
+//     document.write(5 + " X 9 = " + 5 * 9 + "<br/>");
+//     document.write(5 + " X 10 = " + 5 * 10 + "<br/>");
+
+// }
+
+
+
+
+// Question No.5
+
+// var sub1 = prompt("First subject name");
+// var Obt_sub1 = prompt("First Subject Obtained Marks")
+// var sub2 = prompt("Second subject name");
+// var Obt_sub2 = prompt("Second Subject Obtained Marks")
+// var sub3 = prompt("Third subject name");
+// var Obt_sub3 = prompt("Third Subject Obtained Marks")
+// var TotalMarks = 100;
+
+// document.write("<table border=1><tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentsge</th></tr><tr>"+"<td>"+sub1+"</td><td>"+TotalMarks+"</td><td>"+Obt_sub1+"</td><td>"+Obt_sub1/TotalMarks*100+"%"+"</td>"+"</tr><tr>"+"<td>"+sub2+"</td><td>"+TotalMarks+"</td><td>"+Obt_sub2+"</td><td>"+Obt_sub2/TotalMarks*100+"%"+"</td>"+"</tr><tr>"+"<td>"+sub3+"</td><td>"+TotalMarks+"</td><td>"+Obt_sub3+"</td><td>"+Obt_sub3/TotalMarks*100+"%"+"</td>"+"</tr></table>")
+
+
+
+
+
+
+
